5c1d5f8b5790307c1b94ed0f4ed00586eb61fa98
Const <72418922+constvk@users.noreply.github.com>
2022-01-14 18:52:52 -0300

    Add files via upload

---
+++ removertudo.bat
@@ -0,0 +1,6 @@
+@echo off
+echo.                           				                     						
+echo   REMOVER REGISTROS SETADOS   				                                                              							  
+echo.
+reg delete "HKEY_CURRENT_USER\Control Panel\Mouse" /f 
+pause
